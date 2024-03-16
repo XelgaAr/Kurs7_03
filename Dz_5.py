4724e92a3994687e0dbeeb0579d522780709ed38
XelgaAr <143228153+XelgaAr@users.noreply.github.com>
2024-03-16 14:49:02 +0200

    dz_5

--- Dz_5.py
+++ Dz_5.py
@@ -4,21 +4,20 @@
 # [1] => [[1], []]
 # [] => [[], []]
 
-# lst=[1,2,3,4,5,6]
+lst=[1,2,3,4,5,6]
 # lst=[1, 2, 3]
 # lst=[1, 2, 3, 4, 5]
 # lst=[1]
-lst=[]
+# lst=[]
 
+l = int(len(lst) / 2)
 if len(lst) % 2 == 0:
-    l = int(len(lst) / 2)
     lst1 = lst[:l]
     lst2 = lst[l:]
     lst1_lst2 = [lst1, lst2]
     print(lst1_lst2)
-elif len(lst) % 2 != 0:
-    l = int(len(lst) / 2) + 1
-    lst1 = lst[:l]
-    lst2 = lst[l:]
+else:
+    lst1 = lst[:l+1]
+    lst2 = lst[l+1:]
     lst1_lst2 = [lst1, lst2]
     print(lst1_lst2)
